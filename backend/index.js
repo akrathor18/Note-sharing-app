53a333e333827a593d73cfaddee66ced782fdc90
Ashish <127130546+akrathor18@users.noreply.github.com>
2025-04-04 21:29:55 +0530

    creating register and login route

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import db from './config/db.js'
+import users from'./routers/users.js'
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser"; 
 const app = express()
 const port = 3000
+app.use(cookieParser());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/api/users', users)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ backend/routers/users.js
@@ -0,0 +1,60 @@
+import express from "express";
+const router = express.Router();
+import User from '../module/UserSchema.js';
+import jwt from 'jsonwebtoken'
+import VerifyJwtMiddleware from "../Middleware/verifyJWT.js";
+import authMiddleware from "../Middleware/authMiddleware.js";
+import mongoose from "mongoose";
+function generateSessionId(user, res) {
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: "none", 
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
+  return token;
+}
+
+router.post("/register", async (req, res) => {
+    try {
+        const userData = req.body
+        console.log(userData)
+        const userExist = await User.findOne({ email: userData.email })
+        if (userExist) {
+            return res.status(400).json('Email already registered')
+        }
+        const newUser = new User(userData)
+        const resp = await newUser.save()
+        const token = generateSessionId(newUser, res);
+        res.status(201).json({ message: "User registered successfully!", token });
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(`Internal server error`)
+    }
+    
+});
+ router.post("/login", async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const userLogin = await User.findOne({ email });
+
+    if (!userLogin) return res.status(404).json('User not found');
+    const isMatch = await userLogin.comparePassword(password);
+    if (!isMatch) return res.status(400).json('Invalid credentials');
+    const token = generateSessionId(userLogin, res);
+    res.status(201).json({ message: "User registered successfully!", token });
+  
+  } catch (error) {
+    console.log(error)
+    res.status(500).json('Internal server error')
+  }
+});
+
+
+export default router;
