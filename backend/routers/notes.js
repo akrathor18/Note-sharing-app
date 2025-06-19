bab6a45ad34263b13b091abcd8fea38f2dd4713b
Ashish <127130546+akrathor18@users.noreply.github.com>
2025-06-19 12:04:42 +0530

    implementing notes searching

--- backend/routers/notes.js
+++ backend/routers/notes.js
@@ -6,31 +6,53 @@ import authMiddleware from "../Middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post('/upload', verifyJWT, upload.single('file'), async (req, res) => {
-    try {
-      const { title, description, subject } = req.body;
-  
-      const note = await Note.create({
-        title,
-        description,
-        subject,
-        uploadedBy: req.user.id, 
-        fileUrl: req.file.path,
-        fileType: req.file.originalname.split('.').pop(),
-      });
-  
-      res.status(201).json({ success: true, note });
-    } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-  });
+  try {
+    const { title, description, subject } = req.body;
 
-  router.get("/getnotes", authMiddleware, async (req, res)=>{
-    try {
-      const notes = await Note.find();
-      res.status(200).json(notes);
-    } catch (error) {
-      res.status(500).json({ message: 'Failed to retrieve notes', error: error.message });
-    }
-  } )
+    const note = await Note.create({
+      title,
+      description,
+      subject,
+      uploadedBy: req.user.id,
+      fileUrl: req.file.path,
+      fileType: req.file.originalname.split('.').pop(),
+    });
 
-  export default router;
+    res.status(201).json({ success: true, note });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
+router.get("/getnotes", authMiddleware, async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve notes', error: error.message });
+  }
+})
+
+router.get('/search', async (req, res) => {
+  const { search } = req.query;
+
+  const query = search
+    ? {
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { description: { $regex: search, $options: 'i' } },
+        { subject: { $regex: search, $options: 'i' } }
+      ]
+    }
+    : {};
+
+  try {
+    const notes = await Note.find(query);
+    res.status(200).json(notes);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch notes' });
+  }
+});
+
+
+export default router;
